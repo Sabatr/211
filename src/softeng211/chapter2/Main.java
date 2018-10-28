26d1dc4fe1c68acc1d5e967cbf70039662d8c579
Sabatr <bnguyen12152@gmail.com>
2018-10-28 16:30:44 +1300

    Added exercise 2

--- src/softeng211/chapter2/Main.java
+++
@@ -1,8 +0,0 @@
-package softeng211.chapter2;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/softeng211/chapter2/remainder/Main.java
@@ -0,0 +1,12 @@
+package softeng211.chapter2.remainder;
+
+/**
+ * The main of the program. This exercise was from "Lectures on Discrete Mathematics for Computer Science", chapter2 exc2.
+ *
+ * @author Brian Nguyen
+ */
+public class Main {
+    public static void main(String[] args) {
+        new RemainderAndQuotientFinder(20,3);
+    }
+}
